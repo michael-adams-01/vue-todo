d9bd1fd034e0490b0185001bf230be37916c45f9
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-10 15:36:30 -0500

    Add Delete button and add new Task button.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,21 @@
     <button @click="signout" class="p-1 bg-blue-500 font-bold text-white rounded-md">Signout</button>
     <h1 class="text-center text-5xl text-blue-400">Todo List</h1>
     <ul v-for="item in store.todos">
-      <base-card>
+      <base-card class="flex justify-between">
         <li>Title: {{ item.title }}</li>
-        <li>Description: {{ item.description }}</li>
-        <br>
-        <div v-if="!FakeVariable" class="border p-2 border-sky-400"> <!-- For Development only -->
-          <li>Completed: {{ item.completed }}</li>
-          <!-- <li>Due Date: {{ item.dueDate }}</li> -->
-          <!-- <li>Priority: {{ item.priority }}</li> -->
-        </div> <!-- For Development only -->
+        <button class="text-white p-2 border rounded-lg bg-red-500" @click="deleteTodo">X</button>
+        <!-- <li>Completed: {{ item.completed }}</li> -->
+        <!-- <li>Due Date: {{ item.dueDate }}</li> -->
+        <!-- <li>Priority: {{ item.priority }}</li> -->
       </base-card>
     </ul>
+    <base-card class="flex justify-between">
+      <div>
+        <label for="">Add New Task: </label>
+        <input class="border bg-gray-100 w-75" type="text">
+      </div>
+      <button class="text-white p-2 border rounded-lg bg-green-500" @click="deleteTodo">✔</button>
+    </base-card>
   </div>
 </template>
 
@@ -27,6 +31,9 @@ export default {
   methods: {
     signout() {
       this.$router.replace('/auth');
+    },
+    deleteTodo() {
+      console.log('delete Todo ran')
     }
   }
 }
