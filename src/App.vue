019cec5fb33e970c9881f99f9a5d71400abd74d2
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-14 17:00:11 -0500

    Add animations to routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <!-- J.M.J -->
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <router-view v-slot="slotProps">
+    <transition name="fade" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
