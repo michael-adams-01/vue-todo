430aa94ca43a00ce6487abbc630dff046a9415bb
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-16 15:39:21 -0500

    Add BaseModal

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ console.log('Sancte Ioseph, operarius, ora pro nobis');
 import './assets/main.css'
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BaseModal from './components/ui/BaseModal.vue';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -18,5 +19,6 @@ app.use(createPinia())
 app.use(router)
 app.component('BaseCard', BaseCard);
 app.component('BaseSpinner', BaseSpinner);
+app.component('BaseModal', BaseModal);
 
 app.mount('#app')
